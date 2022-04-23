a5fbca0ac29d69bd65d896fe619d7ef18bfad24c
Sfailla <Sfailla1983@gmail.com>
2022-04-22 21:26:23 -0400

    added routes and cors package to main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,27 @@
 import express, { Express, Request, Response } from 'express'
 import dotenv from 'dotenv'
 import logger from 'morgan'
+import cors from 'cors'
 
 import { makeDbConnection } from './db/index.js'
-import { productRoutes } from './routes/index.js'
+import { productRoutes, categoryRoutes } from './routes/index.js'
+import { errorHandler, notFoundHandler } from './middleware/handlers.js'
+
+const isProduction = process.env.NODE_ENV === 'production'
 
 dotenv.config()
 
 const app: Express = express()
 
+app.use(cors())
+app.options('*', cors())
 app.use(logger('dev'))
 app.use(express.json())
 
 makeDbConnection()
 
 app.use('/api/v1/products', productRoutes)
+app.use('/api/v1/categories', categoryRoutes)
 
 app.get('/', (req: Request, res: Response) => {
   const body = req.body
@@ -22,4 +29,16 @@ app.get('/', (req: Request, res: Response) => {
   res.status(200).send('Hello World!')
 })
 
-app.listen(3000, () => console.log('...app running on port 3000!'))
+app.use(notFoundHandler)
+app.use(errorHandler)
+
+// error handling
+if (isProduction) {
+  app.use((err: Error, _req: Request, res: Response) => {
+    res.status(500).json({
+      message: `Something went wrong ${err.message}`
+    })
+  })
+}
+
+app.listen(3000, () => console.log('listening on port 3000'))
