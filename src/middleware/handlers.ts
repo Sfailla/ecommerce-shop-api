17448024e613b07e83a5c806b7fa61c7ae855a26
Sfailla <Sfailla1983@gmail.com>
2022-05-01 08:50:55 -0400

    added middleware for authenticated routes. updated error handler params

--- src/middleware/handlers.ts
+++ src/middleware/handlers.ts
@@ -3,9 +3,7 @@ import { ErrorRequestHandler, NextFunction, Request, Response } from 'express'
 type CustomError = Error & {
   code?: number
   status?: number
-  name?: string
-  message?: string
-  stack?: string
+  value?: string
 }
 
 // catch 404 and forward to error handler
@@ -16,25 +14,17 @@ export const notFoundHandler = (req: Request, res: Response, next: NextFunction)
   next(error)
 }
 
-export const errorHandler = (error: ErrorRequestHandler, req: Request, res: Response): void => {
-  const isDevelopment: boolean = req.app.get('env') === 'development'
-  const { code, status, name, message, stack } = error as unknown as CustomError
+export const errorHandler = (
+  error: ErrorRequestHandler,
+  _req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  // const isDevelopment: boolean = req.app.get('env') === 'development'
+  const { code, status, name, message, value, stack } = error as unknown as CustomError
 
-  // set locals, only providing error in development
-  res.locals.status = status
-  res.locals.code = code
-  res.locals.name = name
-  res.locals.message = message
-  res.locals.error = isDevelopment ? error.toString() : {}
-
-  res.status(status || 500).send({
-    error: {
-      status,
-      code,
-      name,
-      message,
-      stack
-    }
+  res.status(status || 500).json({
+    error: { code, name, status, message, value, stack }
   })
 }
 
